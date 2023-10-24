54cea5c99bbe326efdb27729d3e47b4b95b33d0e
oguzzengin88 <Oguzzengin88@hotmail.com>
2023-10-24 20:23:32 +0300

    Added data layer

---
+++ SRC/SmEticaret/SmEticaret.Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmEticaret.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmEticaret.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CartEntity> Carts { get; set; }
+        public DbSet<CartItemEntity> CartItems { get; set; }
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
+        public DbSet<OrderItemEntity> OrderItems { get; set; }
+        public DbSet<ProductCommentEntity> ProductComments { get; set; }
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<RoleEntity> Roles { get; set; }
+        public DbSet<UserEntity> Users { get; set; }
+
+    }
+}
---
+++ SRC/SmEticaret/SmEticaret.Data/Entities/UserEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmEticaret.Data.Entities
+{
+    public class UserEntity : EntityBase
+    {
+        public int RoleId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
